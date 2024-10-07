7b0d6e3e7c884cee71132944e8f48bbe085ca962
Minhya <anna.nguyen0221@gmail.com>
2024-10-07 16:47:47 +0200

    tried smth

--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -1,16 +1,26 @@
 package org.example.warehouse;
-
 import java.math.BigDecimal;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.function.IntPredicate;
 
 public class Warehouse {
+    private static String myStore;
     Scanner scanner = new Scanner(System.in);
 
     private Warehouse Warehouse;
+    private Warehouse(String myStore) {
+        this.myStore = myStore;
+    }
+
+    // Overloaded private constructor in case no store is provided
+    private Warehouse() {
+        this.myStore = "Default Store"; // You can set a default store name here
+    }
 
     public static Warehouse getInstance(String myStore) {
+        //org.example.warehouse.Warehouse.myStore = myStore;
+
 
     }
 
@@ -18,6 +28,7 @@ public class Warehouse {
     }
 
     public ProductRecord addProduct(UUID uuidMilk, String milk, Category dairy, BigDecimal bigDecimal) {
+
     }
 
     public boolean getProducts() {
